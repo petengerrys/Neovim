559983b86a07949bf776d846327ae150358ae5b2
petengerrys <89357195+petengerrys@users.noreply.github.com>
2022-01-01 17:53:21 -0500

    Update cmp.lua

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -17,31 +17,30 @@ end
 
 --   פּ ﯟ   some other good icons
 local kind_icons = {
-  Text = "",
-  Method = "m",
-  Function = "",
-  Constructor = "",
-  Field = "",
-  Variable = "",
-  Class = "",
-  Interface = "",
-  Module = "",
-  Property = "",
-  Unit = "",
-  Value = "",
-  Enum = "",
-  Keyword = "",
-  Snippet = "",
-  Color = "",
-  File = "",
-  Reference = "",
-  Folder = "",
-  EnumMember = "",
-  Constant = "",
-  Struct = "",
-  Event = "",
-  Operator = "",
-  TypeParameter = "",
+Text = "",
+Method = "m",
+Constructor = "",
+Field = "",  
+Variable = "",
+Class = "",
+Interface = "",
+Module = "",
+Property = "",
+Value = "",
+Enum = "",
+Keyword = "",
+Snippet = "",
+Color = "",
+File = "",
+Reference = "",
+Folder = "",
+EnumMember = "",
+Constant = "",
+Struct = "",
+Event = "",
+Operator = "",
+TypeParameter = "",
+
 }
 -- find more here: https://www.nerdfonts.com/cheat-sheet
 
@@ -99,7 +98,7 @@ cmp.setup {
     format = function(entry, vim_item)
       -- Kind icons
       vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
-      -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+      vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
       vim_item.menu = ({
         luasnip = "[Snippet]",
         buffer = "[Buffer]",
