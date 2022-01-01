80647b10a9257f3ea96417f0cd641763f87dfd73
petengerrys <89357195+petengerrys@users.noreply.github.com>
2022-01-01 13:15:30 -0500

    Update colorscheme.lua

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -6,3 +6,16 @@ catch /^Vim\%((\a\+)\)\=:E185/
   set background=dark
 endtry
 ]]
+
+--local colorscheme = "tokyonight"
+
+--vim.g.tokyonight_style = "night"
+
+-- Change the "hint" color to the "orange" color, and make the "error" color bright red 
+--vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
+
+--local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+--if not status_ok then
+ -- vim.notify("colorscheme " .. colorscheme .. " not found!")
+ -- return
+--end
