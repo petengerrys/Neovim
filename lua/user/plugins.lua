982a091020123feb1650b3360a207f5f449ec2b6
petengerrys <89357195+petengerrys@users.noreply.github.com>
2022-01-02 11:00:40 -0500

    Update plugins.lua

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -45,9 +45,8 @@ return packer.startup(function(use)
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
 
-  -- Colorschemes
-  -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
-  use "lunarvim/darkplus.nvim"
+ -- Colorschemes
+  use "folke/tokyonight.nvim"  
 
   -- cmp plugins
   use "hrsh7th/nvim-cmp" -- The completion plugin
