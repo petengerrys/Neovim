638a1dfd2fa9484e154897b989f07d29ef34c941
petengerrys <89357195+petengerrys@users.noreply.github.com>
2022-01-02 10:58:55 -0500

    Update colorscheme.lua

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,6 +1,12 @@
+
+vim.g.tokyonight_style = "night"
+
+-- Change the "hint" color to the "orange" color, and make the "error" color bright red 
+vim.g.tokyonight_colors = { hint = "orange", error = "#ff0000" }
+
 vim.cmd [[
 try
-  colorscheme darkplus
+  colorscheme tokyonight
 catch /^Vim\%((\a\+)\)\=:E185/
   colorscheme default
   set background=dark
