7fa73eb6abbb7ef7a44eed0aa6bd4a1dcd8fd90d
petengerrys <89357195+petengerrys@users.noreply.github.com>
2022-01-03 20:53:01 -0500

    Update plugins.lua

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -45,10 +45,9 @@ return packer.startup(function(use)
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
   use "windwp/nvim-autopairs" -- Autopairs, integrates with both cmp and treesitter
-
-  -- Colorschemes
-  -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
-  use "lunarvim/darkplus.nvim"
+  
+ -- Colorschemes
+  use "folke/tokyonight.nvim"  
 
   -- cmp plugins
   use "hrsh7th/nvim-cmp" -- The completion plugin
@@ -75,7 +74,10 @@ return packer.startup(function(use)
     "nvim-treesitter/nvim-treesitter",
     run = ":TSUpdate",
   }
-
+  use "p00f/nvim-ts-rainbow"
+  use "nvim-treesitter/playground"
+  use "tomlion/vim-solidity"
+    
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
