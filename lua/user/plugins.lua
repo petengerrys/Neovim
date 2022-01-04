bc9ea6169e1061b2dcb43fd4294dab75feb10ff6
petengerrys <89357195+petengerrys@users.noreply.github.com>
2022-01-04 17:24:11 -0500

    Update plugins.lua

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -51,9 +51,8 @@ return packer.startup(function(use)
   use "akinsho/bufferline.nvim"
   use "moll/vim-bbye"
 
-  -- Colorschemes
-  -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
-  use "lunarvim/darkplus.nvim"
+ -- Colorschemes
+  use "folke/tokyonight.nvim"  
 
   -- cmp plugins
   use "hrsh7th/nvim-cmp" -- The completion plugin
@@ -81,6 +80,9 @@ return packer.startup(function(use)
     run = ":TSUpdate",
   }
   use "JoosepAlviste/nvim-ts-context-commentstring"
+  use "p00f/nvim-ts-rainbow"
+  use "nvim-treesitter/playground"
+  use "tomlion/vim-solidity"
 
   -- Git
   use "lewis6991/gitsigns.nvim"
